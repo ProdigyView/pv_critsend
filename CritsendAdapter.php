3959b0e17734b96588b7c044683e5a4f03b9776f
ProdigyView <contact@prodigyview.com>
2012-06-02 15:54:58 -0400

    Adding documentation for the adapter

--- CritsendAdapter.php
+++ CritsendAdapter.php
@@ -1,3 +1,6 @@
 <?php
-
+/**
+ * The adapter is used to ovveride the PVMail sendEmail method and ensure that all outgoing mail use Critsend.
+ * The adapter is loosely coupled and can be easily turned off by commenting it out below.
+ */
 PVMail::addAdapter('PVMail', 'sendEmail', 'Critsend');
